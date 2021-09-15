61698cbc77a435f083b7cc02993565b9e8072d3d
vishalvenkat23 <vvenka52@asu.edu>
2021-09-14 20:07:38 -0700

    Problem 2b Gradient descent 1.1

--- Homework/Problem_2_Gradient_descent.py
+++ Homework/Problem_2_Gradient_descent.py
@@ -1,6 +1,8 @@
 import numpy as np
 # obj = lambda x: (x[0]+1)**2 + (x[1])**2 + (x[2] - 1)**2  # note that this is 1D. In Prob 2 it should be 2D.
-obj = lambda x: (x[0])**2 + 12*x[0]*x[1] - 8*x[0] + 10*x[1]**2 + 4 - 14*x[1] # 2D
+# obj = lambda x: (x[0])**2 + 12*x[0]*x[1] - 8*x[0] + 10*x[1]**2 + 4 - 14*x[1] # 2D
+def f(x):
+    return (x[0])**2 + 12*x[0]*x[1] - 8*x[0] + 10*x[1]**2 + 4 - 14*x[1]
 
 def grad(x):
     return [10*x[0] + 12*x[1] - 8, 12*x[0] + 20*x[1] - 14]  # correct gradient!
@@ -10,21 +12,23 @@ k = 0  # counter
 soln = [x0]  # use an array to store the search steps
 x = soln[k]  # start with the initial guess
 # error = abs(grad(x))  # compute the error. Note you will need to compute the norm for 2D grads, rather than the absolute value
-error = np.linalg.norm(grad(x))
+error = np.linalg.norm(grad(x), ord=None, axis=None)
+# error = abs(grad(x))
 a = float(0.01)  # set a fixed step size to start with
 
 # Armijo line search
 def line_search(x):
     a = 1.  # initialize step size
-    d = -grad(x)
-    phi = lambda a, x: obj(x) - a*0.8*(np.transpose(grad(x))*d  # define phi as a search criterion
-    while phi(a,x) < obj(x-a*grad(x)):  # if f(x+a*d)>phi(a) then backtrack. d is the search direction
+    d = - grad(x)
+    def phi(a, x):
+        return f(x) - a * 0.8 * (np.transpose(grad(x)) * d  # define phi as a search criterion
+    while phi(a) < f(x+a*d):  # if f(x+a*d)>phi(a) then backtrack. d is the search direction
         a = 0.5*a
     return a
 
 while error >= eps:  # keep searching while gradient norm is larger than eps
-    # a = line_search(x)
-    x = x - a * grad(x)
+    a = line_search(x)
+    x = float(x - a * grad(x))
     soln.append(x)
     error = np.linalg.norm(grad(x))
 
