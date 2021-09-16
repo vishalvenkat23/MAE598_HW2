a12a2069eeab9d206c1169889acc3960488017d4
vishalvenkat23 <vvenka52@asu.edu>
2021-09-15 19:02:58 -0700

    Solution for 2B gradient solution

--- Homework/Trial_code_for_P2b.py
+++ Homework/Trial_code_for_P2b.py
@@ -1,7 +1,7 @@
+# Code for Gradient descent
 import numpy as np
 import scipy
 from scipy.optimize import optimize
-
 
 # eps = 1e-3  # termination cov_cri
 # x_0 = [0, 0]  # initial
@@ -11,22 +11,20 @@ from scipy.optimize import optimize
 # error = 1  # conv
 # a = 0.0001  # alpha
 
-
 def f(x):
     return 5 * x[0] ** 2 + 12 * x[0] * x[1] - 8 * x[1] + 10 * x[1] ** 2 - 14 * x[1] + 4
 
 
-# grad_d(x):
-# return np.array([10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14])
+def grad(x):
+    return np.array([10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14])
 # return [10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14]
-def grad_d(grad, start, learn_r, tol):
-    pass
+# def grad_d(grad, start, learn_r, tol):
+#     pass
 
-
-grad_d(
-    grad=lambda x: np.array([10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14]),
-    start=np.array([0, 0]), learn_r=0.2, tol=1e-3
-)
+# grad_d(
+#     grad=lambda x: np.array([10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14]),
+#     start=np.array([0, 0]), learn_r=0.2, tol=1e-3
+# )
 
 eps = 1e-3
 x0 = (0, 0)
@@ -36,7 +34,7 @@ x = soln[k]
 
 # error = np.linalg.norm([10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14])
 # error = np.linalg.norm(grad(x))
-error = scipy.linalg.norm(grad_d(x))
+error = scipy.linalg.norm(grad(x))
 a = 0.01
 
 
@@ -46,13 +44,11 @@ a = 0.01
 # line search code starts here
 
 def line_search(x):
-    a = 1
-
-    # d = -1 * grad(x)
+    a = 0.01
+    d = -1 * grad(x)
     def phi(a, x):
-        return f(x) - a * 0.8 * grad_d(x) * grad_d(x)
-        # return f(x) - a * 0.8 * np.matmul(np.transpose(grad(x)), d)
-
+#         return f(x) - a * 0.8 * grad(x) * grad(x)
+        return f(x) - a * 0.8 * np.matmul(np.transpose(grad(x)), d)
     # print(phi(x))
     while phi(a, x) < f(x - a * grad(x)):
         a = 0.5 * a
