57466d19072467e8f45b4fbe9bde6b4bfc129044
vishalvenkat23 <vvenka52@asu.edu>
2021-09-15 23:01:35 -0700

    2B gradient no error 1.0 2B Newton's no error 1.0 plots done

--- Homework/Trial_code_for_P2b.py
+++ Homework/Trial_code_for_P2b.py
@@ -1,15 +1,86 @@
-# Code for Gradient descent
+# # Code for Gradient descent
+# import numpy as np
+# import scipy
+# from scipy.optimize import optimize
+#
+# # eps = 1e-3  # termination cov_cri
+# # x_0 = [0, 0]  # initial
+# # k = 1  # count
+# # soln = []  # err
+# # itr = []  # itt
+# # error = 1  # conv
+# # a = 0.0001  # alpha
+#
+# def f(x):
+#     return 5 * x[0] ** 2 + 12 * x[0] * x[1] - 8 * x[1] + 10 * x[1] ** 2 - 14 * x[1] + 4
+#
+#
+# def grad(x):
+#     return np.array([10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14])
+# # return [10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14]
+# # def grad_d(grad, start, learn_r, tol):
+# #     pass
+#
+# # grad_d(
+# #     grad=lambda x: np.array([10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14]),
+# #     start=np.array([0, 0]), learn_r=0.2, tol=1e-3
+# # )
+#
+# eps = 1e-3
+# x0 = (0, 0)
+# k = 0
+# soln = [x0]
+# x = soln[k]
+#
+# # error = np.linalg.norm([10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14])
+# # error = np.linalg.norm(grad(x))
+# error = scipy.linalg.norm(grad(x))
+# a = 0.01
+#
+#
+# # print(error)
+#
+#
+# # line search code starts here
+#
+# def line_search(x):
+#     a = 0.01
+#     d = -1 * grad(x)
+#     def phi(a, x):
+# #         return f(x) - a * 0.8 * grad(x) * grad(x)
+#         return f(x) - a * 0.8 * np.matmul(np.transpose(grad(x)), d)
+#     # print(phi(x))
+#     while phi(a, x) < f(x - a * grad(x)):
+#         a = 0.5 * a
+#     return a
+#
+#     # while phi(a, x) > f(x - a * np.array(grad(x))):
+#     #     a = 0.5 * a
+#
+#
+# while error >= eps:
+#     a = line_search(x)
+#     x = x - a * grad(x)
+#     # x = x - np.matmul(grad(x), a)
+#     soln.append(x)
+#     error = scipy.linalg.norm(grad(x))
+#     # error = np.linalg.norm(x_0 - x_k)
+#     # conv = np.linalg.norm(grad(x_0))
+#     # soln.append(float(error))
+#
+#     # soln.append(x)
+#     # error = np.linalg.norm(g(x))
+# # x = np.array([-2 * x_0[0] - 3 * x_0[1] + 1, x_0[0], x_0[0]])
+# # print(x)
+# print(soln)
+
+# Newton's algorithm
+# Code for Newton's algorithm
 import numpy as np
 import scipy
 from scipy.optimize import optimize
+import matplotlib.pyplot as plt
 
-# eps = 1e-3  # termination cov_cri
-# x_0 = [0, 0]  # initial
-# k = 1  # count
-# soln = []  # err
-# itr = []  # itt
-# error = 1  # conv
-# a = 0.0001  # alpha
 
 def f(x):
     return 5 * x[0] ** 2 + 12 * x[0] * x[1] - 8 * x[1] + 10 * x[1] ** 2 - 14 * x[1] + 4
@@ -17,59 +88,42 @@ def f(x):
 
 def grad(x):
     return np.array([10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14])
-# return [10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14]
-# def grad_d(grad, start, learn_r, tol):
-#     pass
 
-# grad_d(
-#     grad=lambda x: np.array([10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14]),
-#     start=np.array([0, 0]), learn_r=0.2, tol=1e-3
-# )
+
+H = np.array([[10, 12], [12, 20]])
+# H = [[10, 12, 0],[12, 20, 0], [0, 0, 0]]
 
 eps = 1e-3
 x0 = (0, 0)
+# x0 = 0
 k = 0
 soln = [x0]
 x = soln[k]
 
-# error = np.linalg.norm([10 * x[0] + 12 * x[1] - 8, 12 * x[0] + 20 * x[1] - 14])
-# error = np.linalg.norm(grad(x))
 error = scipy.linalg.norm(grad(x))
 a = 0.01
 
-
-# print(error)
-
-
 # line search code starts here
 
-def line_search(x):
-    a = 0.01
-    d = -1 * grad(x)
-    def phi(a, x):
-#         return f(x) - a * 0.8 * grad(x) * grad(x)
-        return f(x) - a * 0.8 * np.matmul(np.transpose(grad(x)), d)
-    # print(phi(x))
-    while phi(a, x) < f(x - a * grad(x)):
-        a = 0.5 * a
-    return a
-
-    # while phi(a, x) > f(x - a * np.array(grad(x))):
-    #     a = 0.5 * a
-
+# def line_search(x):
+#     a = 0.01
+#     d = -1 * grad(x)
+#     def phi(a, x):
+#         return f(x) - a * 0.8 * np.matmul(np.transpose(grad(x)), d)
+#     # print(phi(x))
+#     while phi(a, x) < f(x - a * grad(x)):
+#         a = 0.5 * a
+#     return a
+np.linalg.inv(H)
 
 while error >= eps:
-    a = line_search(x)
-    x = x - a * grad(x)
-    # x = x - np.matmul(grad(x), a)
+    #     a = line_search(x)
+    #     x = x - a * grad(x)
+    x = x - np.matmul(np.linalg.inv(H), grad(x))
+    # x = x - np.linalg.inv(H) * grad(x) # Doesn't work its wrong
     soln.append(x)
     error = scipy.linalg.norm(grad(x))
-    # error = np.linalg.norm(x_0 - x_k)
-    # conv = np.linalg.norm(grad(x_0))
-    # soln.append(float(error))
 
-    # soln.append(x)
-    # error = np.linalg.norm(g(x))
-# x = np.array([-2 * x_0[0] - 3 * x_0[1] + 1, x_0[0], x_0[0]])
-# print(x)
+plt.plot(soln)
+plt.yscale("log")
 print(soln)
